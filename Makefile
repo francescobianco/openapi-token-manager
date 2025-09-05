749a484c5aadb53693cea24da244f98fcc7bfc11
francesco <info.francescobianco@gmail.com>
2025-09-05 12:31:28 +0200

    init tests

--- Makefile
+++ Makefile
@@ -4,3 +4,9 @@ push:
 	@git add .
 	@git commit -am "$$(printf "Enter commit message: " >&2 && read -r message && echo $${message})" || true
 	@git push
+
+test-list:
+	@bash tests/list-test.sh
+
+test-usage:
+	@bash tests/usage-test.sh
--- src/main.sh
+++ src/main.sh
@@ -1,4 +1,85 @@
 
+#module init
+#module sync
+#module pull
+
+usage() {
+  echo "Usage: otm [COMMAND] [OPTIONS]"
+  echo ""
+  echo "Available options"
+  echo "  -S, --sandbox  Execute in the sandbox environment"
+  echo "  -V, --version  Print version info and exit"
+  echo "  -h, --help     Print help information"
+  echo ""
+  echo "Available commands"
+  echo "  new    Create a new token with default settings"
+  echo "  list   List all tokens for the current environment"
+}
+
 main() {
-  echo "Hello World!"
+  local list
+
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -*)
+        case "$1" in
+          --list)
+            list=true
+            ;;
+          -o|--output)
+            echo "Handling $1 with value: $2"
+            shift
+            ;;
+          -V|--version)
+            echo "git-popper ${VERSION}"
+            exit 0
+            ;;
+          -h|--help)
+            usage
+            exit 1
+            ;;
+          *)
+            echo "Unknown option: $1" >&2
+            exit 1
+            ;;
+      esac
+        ;;
+      *)
+        break
+        ;;
+    esac
+    shift
+  done || true
+
+  gitpopper_file="$PWD/.gitpopper"
+
+  case "$1" in
+    init)
+      gitpopper_init "${gitpopper_file}"
+      ;;
+    help)
+      usage
+      exit 1
+      ;;
+  esac
+
+  if [ ! -f "${gitpopper_file}" ]; then
+    echo "No .gitpopper file found in the current directory"
+    exit 1
+  fi
+
+  if [ "$#" -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 1
+  fi
+
+  case "$1" in
+    pull)
+      gitpopper_pull "${gitpopper_file}"
+      ;;
+    sync)
+      gitpopper_init "${gitpopper_file}"
+      gitpopper_sync "${gitpopper_file}"
+      ;;
+  esac
 }
